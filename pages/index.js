5708ef2af1d3613e084adee1b0741620c0392aec
Matt J Raibert <mattraibert@positiondev.com>
2021-03-07 16:49:53 -0500

    switch to server side rite aid requests

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,34 @@
-import Link from 'next/link'
+import {rite_aids} from '../lib/rite-aids'
 
-function Index({ stars }) {
-  return (
-    <div>
-      <p>Next.js has {stars} ⭐️</p>
-      <Link href="/preact-stars">
-        <a>How about preact?</a>
-      </Link>
-    </div>
-  )
+class RiteAidStore {
+  constructor(data) {
+    Object.assign(this, data)
+  }
+
+  hasSlots = () => this.results.Data.slots["1"]
+
+  signUpUrl = "https://www.riteaid.com/pharmacy/covid-qualifier"
 }
 
-export async function getStaticProps() {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const json = await res.json()
 
-  return {
-    props: {
-      stars: json.stargazers_count,
-    },
-  }
+const Index = ({stores, timestamp}) =>
+  <ul>
+    <p>{timestamp}</p>
+    {stores.map((store, i) => {
+      store = new RiteAidStore(store)
+      return <li key={i}>Rite Aid #{store.storeNumber} ({store.fullZipCode}) {store.locationDescription} {store.hasSlots() ?
+        <a href={store.signUpUrl}>⭐ AVAILABLE</a> : null}</li>
+    })}
+  </ul>
+
+export async function getServerSideProps() {
+  const stores = await Promise.all(rite_aids.Data.stores.map(async (store) => {
+    const res = await fetch(`https://www.riteaid.com/services/ext/v2/vaccine/checkSlots?storeNumber=${store.storeNumber}`)
+    store.results = await res.json()
+    return store
+  }))
+
+  return {props: {stores, timestamp: Date.now()}}
 }
 
 export default Index
